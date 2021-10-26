37cceca13b11df5aa5fd94b3978684e4e41c0740
Ch4s3r <lamprecht.patrick1@gmail.com>
2021-10-26 13:03:35 +0200

    Add debugging

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate indoc;
 use std::error::Error;
 use std::fmt::Debug;
 use std::io;
-use std::io::{BufRead, Read, stdin, stdout, Write};
+use std::io::{stdin, stdout, BufRead, Read, Write};
 
 use indoc::indoc;
 
@@ -12,23 +12,21 @@ fn main() {
     eval(&mut stdin().lock(), &mut stdout().lock())
 }
 
-fn get_input(
-    input: &mut (impl Read + BufRead)
-) -> String {
+fn get_input(input: &mut (impl Read + BufRead)) -> String {
     let mut buffer = String::new();
     input.read_line(&mut buffer).expect("Failed");
     buffer.trim().to_string()
 }
 
 struct Food {
-    dog: i32,
-    cat: i32,
+    dog_portions_left: i32,
+    cat_portions_left: i32,
     cat_food_increment: i32,
 }
 
 trait Animal: Debug {
     fn eat(&self, food: &mut Food) -> Result<(), Box<dyn Error>>;
-    fn starving_causes_lose(&self) -> bool;
+    fn starving_causes_loss(&self) -> bool;
 }
 
 #[derive(Debug)]
@@ -36,15 +34,17 @@ struct Cat;
 
 impl Animal for Cat {
     fn eat(&self, food: &mut Food) -> Result<(), Box<dyn Error>> {
-        if food.cat > 0 {
-            food.cat -= 1;
+        if food.cat_portions_left > 0 {
+            food.cat_portions_left -= 1;
+            dbg!("Cat is eating");
             Ok(())
         } else {
+            dbg!("Cat is starving");
             Err("No cat food left.".into())
         }
     }
 
-    fn starving_causes_lose(&self) -> bool {
+    fn starving_causes_loss(&self) -> bool {
         false
     }
 }
@@ -54,52 +54,59 @@ struct Dog;
 
 impl Animal for Dog {
     fn eat(&self, food: &mut Food) -> Result<(), Box<dyn Error>> {
-        if food.dog > 0 {
-            food.dog -= 1;
-            food.cat += food.cat_food_increment;
+        if food.dog_portions_left > 0 {
+            food.dog_portions_left -= 1;
+            food.cat_portions_left += food.cat_food_increment;
+            dbg!("Dog is eating");
             Ok(())
         } else {
+            dbg!("Dog is starving");
             Err("No dog food left.".into())
         }
     }
 
-    fn starving_causes_lose(&self) -> bool {
+    fn starving_causes_loss(&self) -> bool {
         true
     }
 }
 
-fn eval(
-    input: &mut (impl Read + BufRead),
-    output: &mut impl Write,
-) {
+fn eval(input: &mut (impl Read + BufRead), output: &mut impl Write) {
     let number_of_tests = get_input(input).parse::<i32>().unwrap();
     for i in 1..number_of_tests + 1 {
-        let numbers = get_input(input).split(" ").map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>();
+        dbg!("Testcase", i);
+        let numbers = get_input(input)
+            .split(" ")
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect::<Vec<_>>();
         let total_animals = numbers[0];
         let mut food = Food {
-            dog: numbers[1],
-            cat: numbers[2],
+            dog_portions_left: numbers[1],
+            cat_portions_left: numbers[2],
             cat_food_increment: numbers[3],
         };
         let food_chain = get_input(input);
-        let mut result = "YES".to_string();
         let dogs_not_fed = food_chain
             .chars()
-            .map(|animal_char| -> Box<dyn Animal> { if animal_char == 'D' { Box::new(Dog) } else { Box::new(Cat) } })
-            .map(|animal| {
-                match animal.eat(&mut food) {
-                    Ok(_) => { Ok(()) }
-                    Err(e) => { Err(animal) }
+            .map(|animal_char| -> Box<dyn Animal> {
+                if animal_char == 'D' {
+                    Box::new(Dog)
+                } else {
+                    Box::new(Cat)
                 }
             })
-            .filter(|result| {
-                match result {
-                    // todo check how to return true only if the animal is a Dog inside the Box in the Error
-                    // Err(Box(animal, ..)) =>,
-                    _ => { false }
-                }
-            }).count();
+            .map(|animal| animal.eat(&mut food).map_err(|error| animal))
+            .filter(|result| match result {
+                Err(animal) => animal.starving_causes_loss(),
+                _ => false,
+            })
+            .count();
+        // .collect::<Vec<_>>();
 
+        let mut result = if dogs_not_fed > 0 {
+            "NO".to_string()
+        } else {
+            "YES".to_string()
+        };
         output.write(format!("Case #{}: {}\n", i, result).as_bytes());
     }
 }
@@ -119,15 +126,16 @@ mod tests {
             CCCC
             4 2 1 0
             DCCD
-        ".as_bytes();
+        "
+        .as_bytes();
         let mut output: Vec<u8> = Vec::new();
         eval(&mut input, &mut output);
         assert_eq!(
             r"Case #1: YES
 Case #2: YES
 Case #3: NO
-"
-            , from_utf8(output.as_slice()).unwrap()
+",
+            from_utf8(output.as_slice()).unwrap()
         )
     }
 }
